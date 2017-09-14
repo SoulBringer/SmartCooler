bafe37d3281069ec5dad59e533b345c21d29f84f
MikeKoval <mykhailo.koval@elitech.com>
2017-09-14 05:26:25 +0300

    added update shadow lambda

---
+++ UpdateShadowLambda/index.js
@@ -0,0 +1,37 @@
+const AWS = require('aws-sdk');
+
+function getState(cb) {
+  const lambda = new AWS.Lambda();
+  const params = {
+    FunctionName: 'arn:aws:lambda:eu-west-1:091953829232:function:getSmartCoolerState',
+    Payload: '{"httpMethod":"GET"}'
+    };
+  lambda.invoke(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+      return;
+    }
+    const payload = JSON.parse(data.Payload);
+    const body = JSON.parse(payload.body);
+    cb(body.weight);
+  });
+}
+
+function updateShadow(state) {
+  AWS.config.update({region: 'us-east-2'});
+  const iotdata = new AWS.IotData({endpoint: 'a172ulhf1fm9p3.iot.us-east-2.amazonaws.com'});
+  const params = {
+    payload: '{"state": {"reported":{"value":' + state +'}}}',
+    thingName: 'SmartCooler'
+  };
+  iotdata.updateThingShadow(params, (err, data) => {
+    if (err) console.log(err); // an error occurred
+    else console.log(data);           // successful response
+  });
+}
+
+exports.handler = (event, context) => {
+  getState(state => {
+    updateShadow(state);
+  });
+};
