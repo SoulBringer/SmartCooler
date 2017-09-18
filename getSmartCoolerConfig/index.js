822ff9c750acdad594eff39d0851687f12518462
Vitalii Perehonchuk <brute18@gmail.com>
2017-09-18 11:44:07 +0300

    getSmartCoolerConfig changes

--- getSmartCoolerConfig/index.js
+++ getSmartCoolerConfig/index.js
@@ -1,5 +1,10 @@
 const AWS = require('aws-sdk');
-
+/**
+ * AWS' DynamoDB API object
+ * 
+ * @type {AWS.DynamoDB}
+ */
+const dynamodb = new AWS.DynamoDB();
 exports.handler = handler;
 
 function handler(event, context, next) {
@@ -13,12 +18,49 @@ function handler(event, context, next) {
             'Access-Control-Allow-Origin': '*'
         }
     });
+    /**
+     * fetches the config's table name
+     * 
+     * @return {Promise}
+     */
+    function fetchTableName() {
+        let promise = new Promise();
 
-    const table = "smart_cooler_config";
+        dynamodb.listTables({}, (error, data) => {
+            if (error) {
+                console.error(error, error.stack);
+                promise.reject();
+            }
+            else {
+                /**
+                 * A variable for an index of the config table in the array of tables' names.
+                 * 
+                 * @type {Number}
+                 */
+                let table_index = data["TableNames"].find(isConfigTableName);
+                if (table_index === -1) {
+                    console.error("ConfigTable not found.");
+                    promise.reject();
+                }
+                else {
+                    table = data["TableNames"][table_index];
+                    promise.resolve();
+                }
+            }
+        });
+        return promise;
+
+    }
+    /**
+     * The variable for the certain name of the config table in DynamoDB.
+     * 
+     * @type {String}
+     */
+    let table = undefined;
 
     const params = {
         TableName: table,
-        Key:{
+        Key: {
             "id": "0"
         }
     };
@@ -30,11 +72,21 @@ function handler(event, context, next) {
         }
 
         const config = data && data.Item && data.Item.payload;
-    if (!config) {
-        return done('Config is empty' + err);
-    }
-    console.log('Config:', JSON.stringify(config));
+        if (!config) {
+            return done('Config is empty' + err);
+        }
+        console.log('Config:', JSON.stringify(config));
 
-    done(null, config);
-});
+        done(null, config);
+    });
+}
+
+/**
+ * Checks whether a a table is the config table.
+ * 
+ * @param {String} tableName 
+ * @returns {Boolean}
+ */
+function isConfigTableName(tableName) {
+    return tableName.indexOf("ConfigTable") !== -1;
 }
